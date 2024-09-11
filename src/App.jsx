64df49ee1b111214f1c0eb148abea920599f2e45
Muhammad Ausaf Jamal <ausaffarooqui17@gmail.com>
2024-09-11 11:55:52 +0500

    Add Website Under Development Message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './Components/Header/header';
 import Hero from './Components/Hero/hero';
 import Service from './Components/Service/services';
 import About from './Components/About/about';
-import Project  from './Components/Project/Projects';
+import Project from './Components/Project/Projects';
 import Contact from './Components/Contact/contact';
 import Skill from './Components/Skill/skill';
 import ScrollToTopButton from './Components/Topbtn/topbtn';
@@ -13,7 +13,7 @@ import ScrollToTopButton from './Components/Topbtn/topbtn';
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header />
       <Hero />
       <About />
       <Skill/>
@@ -21,7 +21,19 @@ function App() {
       <Project />
       <Contact/>
       <Footer />
-      <ScrollToTopButton/>
+      <ScrollToTopButton/> */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        background: 'white'
+      }}>
+        <h1>
+
+          Website is Currently Under Development.
+          Sorry for the inconvenience
+        </h1>
+      </div>
     </>
   );
 }
